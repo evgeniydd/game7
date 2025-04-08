fa51cfbfb807e286a61914e51bf878cf9594f40c
Evgeniy <evgeniy.duzhenko@gmail.com>
2025-04-08 22:59:26 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-document.querySelector('.nav__burger').addEventListener('click', function () {
-	const burger = document.querySelector('.header__burger');
-	const menu = document.querySelector('.header__menu');
+document.querySelectorAll('.header__burger').forEach((burgerBtn, index) => {
+	const burger = document.querySelectorAll('.header__burger')[index];
+	const menu = document.querySelectorAll('.header__menu')[index];
 
-	burger.addEventListener('click', () => {
+	burgerBtn.addEventListener('click', () => {
 		burger.classList.toggle('header__burger--open');
 		menu.classList.toggle('header__menu--open');
 	});
